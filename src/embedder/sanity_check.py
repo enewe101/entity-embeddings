c4145d5dfa95209f3153184c5e9c9860ad6afc31
Edward Newell <edward.newell@gmail.com>
2016-05-08 08:09:04 -0400

    work on sanity_check

--- src/embedder/sanity_check.py
+++ src/embedder/sanity_check.py
@@ -1,8 +1,9 @@
+import os
 from relation2vec_embedder import Relation2VecEmbedder
 from minibatcher import Relation2VecMinibatcher
 from SETTINGS import DATA_DIR, CORPUS_DIR
 SAVEDIR = os.path.join(DATA_DIR, 'relation2vec')
-
+MIN_FREQUENCY = 20
 
 # load the minibatch generator.  Prune very rare tokens.
 print 'Loading and pruning dictionaries'
@@ -11,7 +12,14 @@ minibatcher.load(SAVEDIR)
 minibatcher.prune(min_frequency=MIN_FREQUENCY)
 
 # Make an embedder with the correct sizes
+print 'Making the embedder'
 embedder = Relation2VecEmbedder(
 	entity_vocab_size=len(minibatcher.entity_dictionary),
 	context_vocab_size=len(minibatcher.context_dictionary),
 )
+print 'Loading previously trained embeddings'
+embeddings_filename = os.path.join(SAVEDIR, 'embeddings.npz')
+embedder.load(embeddings_filename)
+
+print 'Mapping all relation embeddings'
+embedder
