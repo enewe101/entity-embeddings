930f5212fcaaf4f0c707313ea6444c61dbd08723
Edward Newell <edward.newell@gmail.com>
2016-10-17 15:16:18 -0400

    modifications to extract_features

--- src/relational-nouns/extract_features.py
+++ src/relational-nouns/extract_features.py
@@ -1,5 +1,6 @@
+import numpy as np
 import json
-from sklearn import svm
+from sklearn import svm, neighbors
 from iterable_queue import IterableQueue
 import t4k
 from multiprocessing import Process
@@ -14,6 +15,7 @@ from collections import defaultdict, Counter
 from SETTINGS import (
 	RELATIONAL_NOUN_SEEDS_PATH, NONRELATIONAL_NOUN_SEEDS_PATH, FEATURES_PATH
 )
+from nltk.corpus import wordnet
 
 
 NUM_ARTICLE_LOADING_PROCESSES = 12
@@ -31,6 +33,17 @@ def get_seeds():
 	return positive_seeds, negative_seeds
 
 
+class WordNetFeatures(object):
+
+	def __init__(self):
+		pass
+
+	def get_wordnet_features(self, lemma):
+		synsets = wordnet.synsets(lemma)
+		
+
+
+
 def get_fnames():
 
 	# Get the absolute path to all the corenlp files under batch '9fd'
@@ -39,6 +52,11 @@ def get_fnames():
 	fnames = [os.path.join(path, fname) for fname in fnames]
 
 	return fnames
+
+
+def extract_and_save_features(limit=100, path=FEATURES_PATH):
+	features = extract_all_features(limit)
+	open(path, 'w').write(json.dumps(features))
 
 
 def extract_all_features(limit=100):
@@ -89,8 +107,8 @@ def extract_all_features(limit=100):
 	return features
 
 
-def load_features():
-	return json.loads(open(FEATURES_PATH).read())
+def load_features(path=FEATURES_PATH):
+	return json.loads(open(path).read())
 
 
 def get_dictionary(features):
@@ -114,12 +132,12 @@ def get_classifier():
 	dictionary = get_dictionary(features)
 	positive_seeds = filter_seeds(positive_seeds, dictionary)
 	negative_seeds = filter_seeds(negative_seeds, dictionary)
-	classifier = make_classifier(
+	classifier = make_svm_classifier(
 		features, dictionary, positive_seeds, negative_seeds)
 	return classifier, dictionary
 
 
-def cross_val_positives():
+def cross_val_positives(classifier='svm', clf_kwargs={}):
 	positive_seeds, negative_seeds = get_seeds()
 	features = load_features()
 	dictionary = get_dictionary(features)
@@ -132,12 +150,17 @@ def cross_val_positives():
 		positive_seeds_filtered = [
 			p for p in positive_seeds if p is not test_item
 		]
-		classifier = make_classifier(
-			features, dictionary, positive_seeds_filtered, negative_seeds
-		)
+
+		args = (features,dictionary,positive_seeds_filtered,negative_seeds)
+		if classifier == 'svm':
+			clf = make_svm_classifier(*args, clf_kwargs=clf_kwargs)
+		elif classifier == 'knn':
+			clf = make_knn_classifier(*args, clf_kwargs=clf_kwargs)
+		else:
+			raise ValueError('Unexpected classifier type: %s.' % classifier)
 
 		num_tested += 1
-		prediction = classifier.predict(dictionary.get_id(test_item))[0]
+		prediction = clf.predict(dictionary.get_id(test_item))[0]
 		if prediction:
 			num_correct += 1
 
@@ -151,7 +174,7 @@ def cross_val_positives():
 	)
 
 
-def cross_val_negatives():
+def cross_val_negatives(classifier='svm', clf_kwargs={}):
 	positive_seeds, negative_seeds = get_seeds()
 	features = load_features()
 	dictionary = get_dictionary(features)
@@ -164,12 +187,18 @@ def cross_val_negatives():
 		negative_seeds_filtered = [
 			n for n in negative_seeds if n is not test_item
 		]
-		classifier = make_classifier(
-			features, dictionary, positive_seeds, negative_seeds_filtered
-		)
+
+		args = (features,dictionary,positive_seeds,negative_seeds_filtered)
+		if classifier == 'svm':
+			clf = make_svm_classifier(*args, clf_kwargs=clf_kwargs)
+		elif classifier == 'knn':
+			clf = make_knn_classifier(*args, clf_kwargs=clf_kwargs)
+		else:
+			raise ValueError('Unexpected classifier type: %s.' % classifier)
+
 
 		num_tested += 1
-		prediction = classifier.predict(dictionary.get_id(test_item))[0]
+		prediction = clf.predict(dictionary.get_id(test_item))[0]
 		if not prediction:
 			num_correct += 1
 
@@ -183,22 +212,137 @@ def cross_val_negatives():
 	)
 
 
-def make_classifier(features, dictionary, positive_seeds, negative_seeds):
+def make_svm_classifier(
+	features, dictionary, positive_seeds, negative_seeds,
+	clf_kwargs={}
+):
+	'''
+	Make an SVM classifier
+	'''
+	X, Y = make_training_set(dictionary, positive_seeds, negative_seeds)
+	classifier = svm.SVC(kernel=bind_kernel(features, dictionary))
+	classifier.fit(X,Y)
+	return classifier
 
-	# Make the training set.  We're going to use a custom kernel function
-	# so each "row" in the training set has a single "feature" -- it's the
-	# id which identifies the token.  This will let us lookup the 
-	# non-numeric feature in the kernel function
+
+def bind_dist(features, dictionary):
+	def dist(X1, X2):
+		token1, token2 = dictionary.get_tokens([X1[0], X2[0]])
+		features1 = features[token1]
+		features2 = features[token2]
+		dot = dict_dot(features1, features2)
+		if dot == 0:
+			return np.inf
+		return 1 / float(dot)
+	return dist
+
+
+def make_knn_classifier(
+	features, dictionary, positive_seeds, negative_seeds,
+	clf_kwargs={}
+):
+	'''
+	Make a kNN classifier
+	'''
+	X, Y = make_training_set(dictionary, positive_seeds, negative_seeds)
+	mydist = bind_dist(features, dictionary)
+	classifier = KNN(metric=mydist, **clf_kwargs)
+	classifier.fit(X,Y)
+	return classifier
+
+
+class KNN(object):
+
+	def __init__(self, k=3, metric=None):
+		self.k = k
+		self.metric = metric
+		if metric is None:
+			self.metric = self.euclidean
+
+	def euclidean(self, z,x):
+		return np.linalg.norm(z-x)
+
+
+	def fit(self, X, Y):
+		self.X = X
+		self.Y = Y
+
+
+	def predict(self, z):
+
+		if not isinstance(z, np.ndarray):
+			z = np.array(z)
+		if len(z.shape) == 0:
+			z = np.array([[z]])
+		elif len(z.shape) == 1:
+			z = np.array([z])
+		elif len(z.shape) > 2:
+			raise ValueError(
+				'Predict accepts a list of examples whose labels are '
+				'to be predicted, or a single example.  Each examples '
+				'should be a feature vector (or the type accepted by '
+				'distance metric).'
+			)
+
+		# Make predictions for each example
+		predictions = []
+		for row in z:
+			predictions.append(self._predict(row))
+
+		return predictions
+
+
+	def _predict(self, z):
+
+		distances = [np.inf]*self.k
+		labels = [None]*self.k
+
+		# Iterate over all stored examples.  If the distance beats the
+		# stored examples, keep it and the associated label
+		for idx in range(len(self.X)):
+			x, l = self.X[idx], self.Y[idx]
+			d = self.metric(z,x)
+			distances, labels = self.compare(d, l, distances, labels)
+
+		# Now return the majority vote on the label
+		return Counter(labels).most_common(1)[0][0]
+
+
+	def compare(self, d, l, distances, labels):
+		'''
+		compare the distance d to the sorted (ascending) list of distances,
+		starting from the end.  If d is less than any of the items, put it
+		in its proper location (preserving sortedness), then truncate the
+		list back to its original length.  Put l in the corresponding
+		location in labels, and truncate it too.
+		'''
+
+		# First compare to the least element
+		ptr = self.k
+		while ptr > 0 and d < distances[ptr-1]:
+			ptr -= 1
+
+		if ptr < self.k:
+			distances.insert(ptr, d)
+			distances.pop()
+			labels.insert(ptr, l)
+			labels.pop()
+
+		return distances, labels
+
+
+
+def make_training_set(dictionary, positive_seeds, negative_seeds):
+	# Make the training set.  Each "row" in the training set has a single 
+	# "feature" -- it's the id which identifies the token.  
+	# This will let us lookup the non-numeric features in kernel functions
 	X = (
 		[ [dictionary.get_id(s)] for s in positive_seeds]
 		+ [ [dictionary.get_id(s)] for s in negative_seeds]
 	)
 	Y = [True] * len(positive_seeds) + [False] * len(negative_seeds)
 
-	classifier = svm.SVC(kernel=bind_kernel(features, dictionary))
-	classifier.fit(X,Y)
-
-	return classifier
+	return X, Y
 
 
 def bind_kernel(features, dictionary):
@@ -234,7 +378,6 @@ def bind_kernel(features, dictionary):
 	return kernel
 
 
-
 def dict_dot(a,b):
 	result = 0
 	for key in a:
@@ -242,7 +385,6 @@ def dict_dot(a,b):
 			result += a[key] * b[key] / float(a['count'] * b['count'])
 	return result
 		
-
 
 def extract_features_from_articles(fnames_consumer, features_producer):
 
@@ -267,17 +409,18 @@ def extract_features_from_article(article):
 	for sentence in article.sentences:
 		for token in sentence['tokens']:
 
-			# We're only interested in nouns
-			if not token['pos'].startswith('N'):
+			# We're only interested in non-proper nouns
+			if token['pos'] not in ('NN', 'NNS'):
 				continue
 
-			# Add features seen for this instance
+			# Add features seen for this instance.  We use the lemma as
+			# the key around which to aggregate features for the same word
 			add_features = extract_features_from_token(token)
-			features[token['word']] += add_features
+			features[token['lemma']] += add_features
 
 			# We also keep count of the number of times a given token is
 			# seen which helps with normalization of the feature dict
-			features[token['word']]['count'] += 1
+			features[token['lemma']]['count'] += 1
 
 	return features
 
@@ -308,6 +451,4 @@ def extract_features_from_token(token):
 	return add_features
 
 	
-
-
 
